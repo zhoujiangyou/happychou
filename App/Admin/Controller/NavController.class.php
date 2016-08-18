bc4735cab2225339ecfb46137b970dbe37b81774
coder油 <935468215@qq.com>
2016-08-18 12:36:08 +0800

    Add files via upload

---
+++ App/Admin/Controller/NavController.class.php
@@ -0,0 +1,56 @@
+<?php
+// +----------------------------------------------------------------------
+// | Copyright (c) 2015 http://www.happychou.net All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author:  zhouht (zhouht@happychou.net)
+// +----------------------------------------------------------------------
+namespace Admin\Controller;
+class NavController extends CommonController {
+    public function index(){
+		$lists=M('nav')->where(array('pid'=>0))->order('sort desc')->select();
+		foreach($lists as $k=>$v){
+			$lists[$k]['sub']=M('nav')->where(array('pid'=>$v['id']))->order("sort desc")->select();
+		}
+		$this->lists=$lists;
+		$this->display();
+    }
+	public function nav_edit(){
+		
+		$id=I('get.id');
+		if(IS_POST){
+			$post=I('post.');
+			if($id){
+				$post['id']=$id;
+				$ok=M('nav')->save($post);				
+			}else{
+				$ok=M('nav')->add($post);				
+			}
+			if($ok){
+				$this->success('成功');
+			}else{
+				$this->error('失败');
+			}
+		}else{
+			$this->info=M('nav')->where(array('id'=>$id))->find();
+			$this->lists=M('nav')->where(array('pid'=>0))->order('sort desc')->select();
+			$this->display();
+		}
+	
+		
+	}
+	public function nav_del(){
+		$id=I('post.id');
+		if(M('nav')->where(array('pid'=>$id))->find()){
+			$this->error('先删除下级');
+		}else{
+			if(M('nav')->delete($id)){
+				$this->success('删除成功');
+			}else{
+				$this->error('删除失败');
+			}
+		}
+		
+	}
+}
---
+++ App/Admin/Controller/QuestionsController.class.php
@@ -0,0 +1,159 @@
+<?php
+// +----------------------------------------------------------------------
+// | Copyright (c) 2015 http://www.happychou.net All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author:  zhouht (zhouht@happychou.net)
+// +----------------------------------------------------------------------
+namespace Admin\Controller;
+use Think\Page;
+
+class QuestionsController extends CommonController
+{
+    public function index()
+    {
+        $question = D("ItemQuestions");
+        $where = array();
+        $uin = I('get.uin', 0);
+        if($uin!==0){
+            $where['a.uin'] = $uin ;
+        }
+        $count = $question->countList($where);
+        $page = new Page($count, 10);
+        $limit = $page->firstRow . ',' . $Page->listRows;
+        $this->page = $page->show(); // 分页显示输出
+        $list = $question->loadList($where, $limit);
+        foreach($list as $k => $value){
+            $qid = $value['id'];
+            $list[$k]['answers']    =   D("ItemAnswers")->loadList(array('a.qid'=>$qid));
+        }
+        $this->list = $list;
+        $this->display();
+    }
+
+
+    //添加项目问题
+    public function add(){
+        if(IS_POST){
+            $itemQuestion = D("ItemQuestions");
+            if (!$itemQuestion->create()) {
+                $this->error($itemQuestion->getError());
+            } else {
+                if ($itemQuestion->add()) {
+                    $this->success('提交成功！');
+                } else {
+                    $this->error('提交失败！');
+                }
+            }
+        }else{
+            $this->display();
+        }
+
+    }
+
+    //编辑项目问题
+    public function edit(){
+        if(IS_POST){
+            $post=I('post.');
+            $itemQuestion = D("ItemQuestions");
+            $_SESSION['user']['uin'] = $post['uin'];
+            if (!$itemQuestion->create($post,$itemQuestion::MODEL_UPDATE)) {
+                $this->error($itemQuestion->getError());
+            } else {
+                if ($itemQuestion->where("id=".$post['id'])->save($post)) {
+                    unset($_SESSION['user']['uin']);        //绕过model里面的规则检查
+                    $this->success('修改成功！');
+                } else {
+                    $this->error('修改失败！');
+                }
+            }
+        }else{
+            $id = I("id");
+            $info = D("ItemQuestions")->getInfo(array("a.id"=>$id));
+            $this->info = $info;
+            $this->display();
+        }
+
+    }
+
+    //删除项目问题
+    public function del_question(){
+        if(IS_POST){
+            $id = I("post.id");
+            $result = M("item_questions")->where("id='$id'")->delete();
+            $answers = M("item_answers")->where("qid=".$id)->delete();
+            if($result!=false && $answers!=false){
+                $this->success("删除成功！");
+            }else{
+                $this->error("删除失败!");
+            }
+        }else{
+            $this->error("请以正确的方式访问");
+        }
+    }
+
+    //删除项目问题回复
+    public function del_answers(){
+        if(IS_POST){
+            $id = I("post.id");
+            $answers = M("item_answers")->where("id=".$id)->delete();
+            if( $answers!=false){
+                $this->success("删除成功！");
+            }else{
+                $this->error("删除失败!");
+            }
+        }else{
+            $this->error("请以正确的方式访问");
+        }
+    }
+
+
+    //添加问题回复
+    public function add_answers(){
+        if(IS_POST){
+            $post=I('post.');
+            $_SESSION['user']['uin'] = $post['uin'];
+            $itemAnswer = D("ItemAnswers");
+            if (!$itemAnswer->create()) {
+                $this->error($itemAnswer->getError());
+            } else {
+                if ($itemAnswer->add()) {
+                    unset($_SESSION['user']['uin']);        //绕过model里面的规则检查
+                    $this->success('提交成功！');
+                } else {
+                    $this->error('提交失败！');
+                }
+            }
+        }else{
+            $qid = I("qid");
+            $this->qid = $qid;
+            $this->display();
+        }
+
+    }
+
+    //修改问题回复
+    public function edit_answers(){
+        if(IS_POST){
+            $post=I('post.');
+            $_SESSION['user']['uin'] = $post['uin'];
+            $itemAnswer = D("ItemAnswers");
+            if (!$itemAnswer->create($post,$itemAnswer::MODEL_UPDATE)) {
+                $this->error($itemAnswer->getError());
+            } else {
+                if ($itemAnswer->where("id=".$post['id'])->save($post)) {
+                    unset($_SESSION['user']['uin']);        //绕过model里面的规则检查
+                    $this->success('提交成功！');
+                } else {
+                    $this->error('提交失败！');
+                }
+            }
+        }else{
+            $id = I("id");
+            $this->info = D("ItemAnswers")->getInfo(array("a.id"=>$id));
+            $this->display();
+        }
+    }
+
+}
